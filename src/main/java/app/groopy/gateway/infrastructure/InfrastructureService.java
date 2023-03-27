e24adf90128cd5842dcbffafb64ba3656f0bd7ea
alessiodanna <alessio.danna.94@gmail.com>
2023-03-27 20:15:07 +0200

    logs added

--- src/main/java/app/groopy/gateway/infrastructure/InfrastructureService.java
+++ src/main/java/app/groopy/gateway/infrastructure/InfrastructureService.java
@@ -23,34 +23,42 @@ public class InfrastructureService {
     // RoomService calls
     public Message createRoom(RoomServiceProto.CreateRoomRequest req) throws InfrastructureException {
         try {
+            LOGGER.info("sending CreateRoom message to room-service {}", req.toString());
             return roomServiceStub.createRoom(req);
         } catch (Exception e) {
+            LOGGER.error("An error occurred trying to call room-service with: {}", req);
             throw new InfrastructureException(GroopyService.ROOM_SERVICE, e.getLocalizedMessage());
         }
     }
 
     public Message subscribeToRoom(RoomServiceProto.SubscribeRoomRequest req) throws InfrastructureException {
         try {
+            LOGGER.info("sending SubscribeRoom message to room-service {}", req.toString());
             return roomServiceStub.subscribe(req);
         } catch (Exception e) {
+            LOGGER.error("An error occurred trying to call room-service with: {}", req);
             throw new InfrastructureException(GroopyService.ROOM_SERVICE, e.getLocalizedMessage());
         }
     }
 
     public Message searchRooms(RoomServiceProto.ListRoomRequest req) throws InfrastructureException {
         try {
+            LOGGER.info("sending ListRoomRequest message for search scope to room-service {}", req.toString());
             return roomServiceStub.searchRoom(req);
         } catch (Exception e) {
+            LOGGER.error("An error occurred trying to call room-service with: {}", req);
             throw new InfrastructureException(GroopyService.ROOM_SERVICE, e.getLocalizedMessage());
         }
     }
 
     public Message listRoom(GatewayProto.UserRoomsRequest req) throws InfrastructureException {
         try {
+            LOGGER.info("sending ListRoomRequest message for user list rooms scope to room-service {}", req.toString());
             return roomServiceStub.listRoom(RoomServiceProto.ListRoomRequest.newBuilder()
                     .setUserId(req.getUserId())
                     .build());
         } catch (Exception e) {
+            LOGGER.error("An error occurred trying to call room-service with: {}", req);
             throw new InfrastructureException(GroopyService.ROOM_SERVICE, e.getLocalizedMessage());
         }
     }
@@ -58,16 +66,20 @@ public class InfrastructureService {
     // UserService calls
     public Message signUp(UserServiceProto.SignUpRequest req) throws InfrastructureException {
         try {
+            LOGGER.info("sending SignUpRequest message to user-service {}", req.toString());
             return userServiceStub.signUp(req);
         } catch (Exception e) {
+            LOGGER.error("An error occurred trying to call user-service with: {}", req);
             throw new InfrastructureException(GroopyService.USER_SERVICE, e.getLocalizedMessage());
         }
     }
 
     public Message signIn(UserServiceProto.SignInRequest req) throws InfrastructureException {
         try {
+            LOGGER.info("sending SignUpRequest message to user-service {}", req.toString());
             return userServiceStub.signIn(req);
         } catch (Exception e) {
+            LOGGER.error("An error occurred trying to call user-service with: {}", req);
             throw new InfrastructureException(GroopyService.USER_SERVICE, e.getLocalizedMessage());
         }
     }
