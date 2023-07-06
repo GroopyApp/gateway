d9a9136b72aa8b0387908784f78d4fb253da19ba
alessiodanna <alessio.danna.94@gmail.com>
2023-07-06 19:04:10 +0200

    cors

--- src/main/java/app/groopy/gateway/config/CorsConfig.java
+++ src/main/java/app/groopy/gateway/config/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Imposta l'origine consentita (puoi specificare l'URL del client o "*" per consentire da qualsiasi origine)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Imposta i metodi HTTP consentiti
-                .allowedHeaders("*") // Imposta gli header consentiti
-                .allowCredentials(true) // Abilita l'invio di credenziali (se necessario)
-                .maxAge(3600); // Imposta il tempo di memorizzazione nella cache della configurazione CORS
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+//                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
