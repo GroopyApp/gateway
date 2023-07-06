86d253581f62d833b3ce9d2e133b9519ce63cc2f
alessiodanna <alessio.danna.94@gmail.com>
2023-07-06 19:26:12 +0200

    cors

---
+++ src/main/java/app/groopy/gateway/config/CorsFilter.java
@@ -0,0 +1,32 @@
+package app.groopy.gateway.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, x-api-key");
+
+        chain.doFilter(req, res);
+    }
+
+}
--- src/main/java/app/groopy/gateway/config/SecurityConfig.java
+++ src/main/java/app/groopy/gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package app.groopy.gateway.config;
 
 import app.groopy.gateway.config.filters.ApiKeyAuthenticationFilter;
 import app.groopy.gateway.config.filters.AuthTokenFilter;
+import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,15 +22,19 @@ public class SecurityConfig {
     @Autowired
     private AuthTokenFilter authTokenFilter;
 
+    @Autowired
+    private CorsFilter corsFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return  http.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/auth").permitAll()
                         .requestMatchers("/v1/request").permitAll() //TODO change this to authenticated
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(corsFilter, Filter.class)
                 .addFilterBefore(apiKeyAuthenticationFilter, BasicAuthenticationFilter.class)
                 .addFilterBefore(authTokenFilter, BasicAuthenticationFilter.class)
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/v1/request"))
