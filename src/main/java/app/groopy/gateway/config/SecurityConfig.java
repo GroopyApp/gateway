db493cb85ac797e865b7e497f5a5b9f922ddc392
alessiodanna <alessio.danna.94@gmail.com>
2023-07-07 00:34:08 +0200

    cors

--- src/main/java/app/groopy/gateway/config/SecurityConfig.java
+++ src/main/java/app/groopy/gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package app.groopy.gateway.config;
 
 import app.groopy.gateway.config.filters.ApiKeyAuthenticationFilter;
 import app.groopy.gateway.config.filters.AuthTokenFilter;
+import app.groopy.gateway.config.filters.CorsServiceFilter;
 import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,18 +25,18 @@ public class SecurityConfig {
     private AuthTokenFilter authTokenFilter;
 
     @Autowired
-    private CorsFilter corsFilter;
+    private CorsServiceFilter corsFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return  http.csrf(AbstractHttpConfigurer::disable)
-//                .cors(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/auth").permitAll()
                         .requestMatchers("/v1/request").permitAll() //TODO change this to authenticated
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(corsFilter, Filter.class)
+                .addFilterBefore(corsFilter, CorsFilter.class)
                 .addFilterBefore(apiKeyAuthenticationFilter, BasicAuthenticationFilter.class)
                 .addFilterBefore(authTokenFilter, BasicAuthenticationFilter.class)
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/v1/request"))
--- src/main/java/app/groopy/gateway/config/filters/CorsServiceFilter.java
+++ src/main/java/app/groopy/gateway/config/filters/CorsServiceFilter.java
@@ -1,24 +1,26 @@
-package app.groopy.gateway.config;
+package app.groopy.gateway.config.filters;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
+public class CorsServiceFilter extends OncePerRequestFilter {
 
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 //        response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -26,7 +28,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, x-api-key");
 
-        chain.doFilter(req, res);
+        filterChain.doFilter(request, response);
     }
 
 }
